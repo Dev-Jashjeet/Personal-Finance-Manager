ddd1fd469796aeae8531f23f56d75cbf972558b5
Jashjeet Singh <jashjeet99@gmail.com>
2026-04-12 20:59:57 +0530

    login functionality added

--- login.js
+++ login.js
@@ -1,1 +1,49 @@
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+const button = document.querySelector(".login-btn");
+let emailPassError = document.querySelectorAll(".error-message");
+let loader = document.querySelector(".loader-cont");
+;
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let count = 0;
+    if (email.value !== "" && password.value !== "") {
+        loader.style.display = "flex";
+        setTimeout(() => {
+            if (localStorage.getItem("usersDetails") !== null) {
+                const userDetailsStr = localStorage.getItem("usersDetails");
+                const userDetailsObj = JSON.parse(userDetailsStr);
+                for (let userDetail of userDetailsObj) {
+                    if (userDetail.email === email.value && userDetail.password === password.value) {
+                        loader.style.display = "none";
+                        alert("Access Guranteed");
+                        window.location.replace("dashboard.html");
+                        count++;
+                        return;
+                    }
+                }
+                if (count === 0) {
+                    loader.style.display = "none";
+                    email.value = "";
+                    password.value = "";
+                    alert("User not found, please sign up");
+                    return;
+                }
+            }
+            else {
+                loader.style.display = "none";
+                email.value = "";
+                password.value = "";
+                alert("USER NOT FOUND PLEASE SIGN UP");
+                return;
+            }
+        }, 2000);
+    }
+    else {
+        emailPassError[0].style.opacity = "1";
+        emailPassError[1].style.opacity = "1";
+    }
+    return;
+});
 export {};
+// Completed
