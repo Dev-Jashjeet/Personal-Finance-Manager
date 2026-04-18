c3e31edb06f66a5b6abf54d31459879b01f6c5c1
Jashjeet Singh <jashjeet99@gmail.com>
2026-04-18 18:19:28 +0530

    reload and logout functionality added

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,28 @@
-
+let userProfileName = document.querySelectorAll(".userprofile");
+let logoutBtn = document.querySelectorAll(".logout-btn");
+let BMESCont = document.querySelectorAll(".BMEScont");
+let Person;
+// Function that return Login User object
+function getUserData() {
+    const loginUserEmail = sessionStorage.getItem("loginemail");
+    const usersDetails = JSON.parse(localStorage.getItem("usersDetails"));
+    for (let users of usersDetails) {
+        if (users.email === loginUserEmail) {
+            return users;
+        }
+    }
+    throw new Error("User not found");
+}
+// Function when page reloads
+(function reload() {
+    Person = getUserData();
+    userProfileName[0].innerHTML = Person.name;
+    userProfileName[1].innerHTML = Person.email;
+    userProfileName[2].innerHTML = `Welcome, ${(Person.name).split(" ")[0]}!`;
+})();
+//Function when user click logout
+logoutBtn[1].addEventListener('click', () => {
+    sessionStorage.clear();
+    window.location.replace("index.html");
+});
+export {};
